8a78b4d02489fc30a4867e2685e148dbb4c4d528
0xvikasrushi <0xvikas@gmail.com>
2023-04-15 15:18:27 +0530

    ts converted

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
-import { v4 as uuidv4 } from 'uuid'
-import express from 'express';
-import { RaftNode } from './raft-node';
-import { PubSub } from './pubSub';
+import { v4 as uuidv4 } from "uuid";
+import express from "express";
+import { RaftNode } from "./raft-node";
+import { PubSub } from "./pubSub";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +14,30 @@ raftNode.votingProcedure();
 
 app.get("/", function (req, res) {
   res.send("working");
+});
+
+app.post("/requestvote", async (req, res) => {
+  try {
+    const { term, candidateId, lastLogIndex, lastLogTerm } = req.body;
+    res.json({ term: term, voteGranted: true });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+});
+app.post("/appendEntries", async (req, res) => {
+  try {
+    const {
+      term,
+      leaderId,
+      prevLogIndex,
+      prevLogTerm,
+      entries: { term: term1, command: command1 },
+      leaderCommit,
+    } = req.body;
+    res.json({ term: term, success: true });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
 });
 
 app.listen(PORT, () => {
