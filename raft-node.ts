4bd1794cec0ec3eda7b25cbc9def0c0d8c284349
Shubh Chudasama <shubh.cs@proton.me>
2023-04-15 17:32:22 +0000

    Implement timer

--- raft-node.ts
+++ raft-node.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { getTimeout, countTrue, countVotes } from "./utils";
+import { Timer } from "./timer";
 
 export enum MessageType {
   gatherVotes = "gatherVotes",
@@ -25,19 +26,30 @@ export class RaftNode {
   peer: string[];
 
   //  -----
-  timeoutId: any;
+  timer: Timer;
 
   constructor(id: string) {
+    this.timer = new Timer();
     this.id = id;
     this.votedFor = null;
     this.currentTerm = 0;
     this.log = [];
     this.type = NodeType.follower;
     this.peer = ["3001", "3002", "3003", "3004"].filter((p) => p !== this.id);
+
+    this.timer.start(getTimeout());
+    console.log(this.timer.randTime);
+
+    this.timer.on("timeout", this.handleTimeout.bind(this));
   }
 
-  clearTimeoutVoting() {
-    clearTimeout(this.timeoutId);
+  handleTimeout() {
+    this.timer.stop();
+
+    // becomes candidate
+    this.type = NodeType.candidate;
+
+    this.votingProcedure();
   }
 
   async sendHeartBeats() {
@@ -57,39 +69,29 @@ export class RaftNode {
   async votingProcedure() {
     this.currentTerm++;
     this.votedFor = this.id;
-    // there's a time out
-    const timeout = getTimeout();
-    console.log({ timeout });
-    this.timeoutId = setTimeout(() => {
-      // after timeout
-
-      // becomes candidate
-      this.type = NodeType.candidate;
-      const promises = this.peer.map((p) => {
-        return axios.post(`http://localhost:${p}/requestVote`, {
-          term: this.currentTerm,
-          candidateId: this.id,
-          lastLogIndex: this.log.length - 1,
-          lastLogTerm: this.log[this.log.length - 1]?.term,
-        });
+    const promises = this.peer.map((p) => {
+      return axios.post(`http://localhost:${p}/requestVote`, {
+        term: this.currentTerm,
+        candidateId: this.id,
+        lastLogIndex: this.log.length - 1,
+        lastLogTerm: this.log[this.log.length - 1]?.term,
       });
+    });
 
-      Promise.all(promises).then((results) => {
-        const res = results.map((r) => r.data);
-        console.log(res);
+    Promise.all(promises).then((results) => {
+      const res = results.map((r) => r.data);
+      console.log(res);
 
-        const votes = countVotes(res);
-        console.log(votes);
+      const votes = countVotes(res);
+      console.log(votes);
 
-        if (votes.trueCount > votes.falseCount) {
-          this.type = NodeType.leader;
-        }
-        if ((this.type = NodeType.leader)) {
-          this.sendHeartBeats();
-        }
-      });
-    }, timeout);
-
+      if (votes.trueCount > votes.falseCount) {
+        this.type = NodeType.leader;
+      }
+      if ((this.type = NodeType.leader)) {
+        this.sendHeartBeats();
+      }
+    });
     // await this.sendHeartBeats();
     // asks for votes
     // ...
