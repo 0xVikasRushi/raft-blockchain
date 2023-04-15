d28ddb4861afd8a9048cf2ff7840175f6c7949ea
0xvikasrushi <0xvikas@gmail.com>
2023-04-15 15:32:45 +0530

    phase 1 done

--- server.ts
+++ server.ts
@@ -12,6 +12,7 @@ export const pubsub = new PubSub();
 const raftNode = new RaftNode(nodeId);
 raftNode.votingProcedure();
 
+// TODO TRY CATCH VALIDATION
 app.get("/", function (req, res) {
   res.send("working");
 });
@@ -34,12 +35,24 @@ app.post("/appendEntries", async (req, res) => {
       entries: { term: term1, command: command1 },
       leaderCommit,
     } = req.body;
+
     res.json({ term: term, success: true });
   } catch (error) {
     res.status(404).json({ error: error });
   }
 });
 
+app.post("/executeCommand", async (req, res) => {
+  try {
+    const { command } = req.body;
+    if (!command) {
+      return res.status(404).json({ message: "command not found" });
+    }
+    return res.send(command);
+  } catch (error) {
+    return res.status(404).json({ error: error });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
 });
